96232cd9d88462526b21416f34c1d25fff25a382
Zane <labs.zane@gmail.com>
2025-12-05 00:02:27 +0100

    Adding the first code base

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import discord 
+from dotenv_vault import load_dotenv
+import os
+from discord.ext import commands
+load_dotenv()
 
-print('Hello, Discord !')
+TOKEN = os.getenv("TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+bot.run(TOKEN)
