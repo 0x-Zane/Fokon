dffb80199b3134d165baca408b8b8034e03bf513
Zane <labs.zane@gmail.com>
2025-12-07 13:37:21 +0100

    Replacing discord.Client with commands.Bot to make slash commands more efficent in the future

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import discord 
-from dotenv_vault import load_dotenv
-import os
 from discord.ext import commands
+import os
+from dotenv_vault import load_dotenv
+ 
+    #DEFINING INTENTS
+bot_intents = discord.Intents.default()
+bot_intents.message_content = True
+
+ #Get the token from .env file
 load_dotenv()
-
 TOKEN = os.getenv("TOKEN")
-intents = discord.Intents.default()
-intents.message_content = True
+
+client = commands.Bot(command_prefix= "/",intents=bot_intents)
 
 
-bot = commands.Bot(command_prefix="/", intents=intents)
+#2 MAIN WAYS TO CREATE A DISCORD BOT:
 
-bot.run(TOKEN)
+# 1.FROM DISCORD.EXT USING COMMANDS
+# 2.CREATING DIRECTLY A BOT OBJECT WITH:  DISCORD.CLIENT()
+
+
+@client.event #
+async def  on_ready():
+    print(f"The bot is now ready for use as {client.user} ")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello, World!")
+
+
+client.run(TOKEN)
+
+
+
